b6456621b032d047c465fe2b688f5e6efd3b1704
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-07-07 11:25:34 -0300

    Modelling the Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,17 +3,55 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour must have a name'],
+    required: [true, 'A tour must have a name.'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration.'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size.'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty.'],
+    trim: true,
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price'],
+    required: [true, 'A tour must have a price.'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a description.'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image.'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
