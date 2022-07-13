bc78c2f7fd06f7549dd2632be301855c2955d451
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-07-13 15:38:01 -0300

    Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,13 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value:  ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -47,7 +54,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
-    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     // if (error.name === 'ValidationError')
     //   error = handleValidationErrorDB(error);
 
