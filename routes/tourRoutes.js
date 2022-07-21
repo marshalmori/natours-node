671f0ebba1f5e9af21b2275d538e61b1e862d699
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-07-20 22:14:47 -0300

    Nested Routes with Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -27,18 +29,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-// POST /tour/234569adef3/reviews
-// GET /tour/234569adef3/reviews
-// GET /tour/234569adef3/reviews/95738adef4
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
