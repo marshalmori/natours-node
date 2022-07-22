2aee46b42c24fd2de211630d79ccd4f14fb9015f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-07-21 22:08:29 -0300

    Adding a /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,18 +11,10 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-// exports.getAllUsers = catchAsync(async (req, res, next) => {
-//   const users = await User.find();
-
-//   // SEND RESPONSE
-//   res.status(200).json({
-//     status: 'success',
-//     results: users.length,
-//     data: {
-//       users,
-//     },
-//   });
-// });
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTS password data
