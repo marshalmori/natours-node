90a7ed13e0f4575d477ff927b2acd16bd297b8dd
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-07-26 23:22:55 -0300

    Geospatial Queries: finding Tours within radius

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -23,6 +23,10 @@ router
   );
 
 router
+  .route('/tours-within/:distance/center/:latlong/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .patch(
