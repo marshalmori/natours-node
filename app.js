532e0fce648a8716edea66f4cc114caedfb29e1e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-08-10 15:01:06 -0300

    Install CORS

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const xss = require('xss-clean');
 
 const cookieParser = require('cookie-parser');
 
+const cors = require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -18,6 +19,18 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) MIDDLEWARES
+// Implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin *
+// api.natours.com, natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   })
+// );
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
